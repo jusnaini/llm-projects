0f2c7b6a22a455b37c30b29dc2ac6ce9eaa0fdc9
juzzms <jusnainimuslimin@gmail.com>
2025-11-16 10:42:19 +0800

    remove deprecated

--- deprecated/backend/settings.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
-if not MISTRAL_API_KEY:
-    raise EnvironmentError("Missing MISTRAL_API_KEY in environment variables")
